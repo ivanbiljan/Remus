b3b07b5224d75662248db94de41d4418e729ee0d
ivanbiljan <ssbiljan@gmail.com>
2020-10-15 03:00:24 +0200

    Implement ConsoleSender

---
+++ Remus/ConsoleSender.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remus {
+    /// <summary>
+    /// Represents a CLI sender.
+    /// </summary>
+    [PublicAPI]
+    public sealed class ConsoleSender : ICommandSender {
+        public void SendMessage(string message) => Console.WriteLine(message);
+    }
+}
