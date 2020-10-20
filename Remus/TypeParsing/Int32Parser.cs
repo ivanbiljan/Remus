6199862b631ca4f6bfa54ccbfdd44f85a22fd636
ivanbiljan <ssbiljan@gmail.com>
2020-10-20 21:03:53 +0200

    Add Int32Parser

---
+++ Remus/TypeParsing/Int32Parser.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remus.TypeParsing {
+    internal sealed class Int32Parser : ITypeParser<int> {
+        public int Parse(string input) => int.Parse(input);
+    }
+}
