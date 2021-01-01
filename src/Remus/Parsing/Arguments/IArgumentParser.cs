ba27cbad9474034d2c3deb4ab37aa8f773bd1993
ivanbiljan <ssbiljan@gmail.com>
2021-01-01 23:57:33 +0100

    Add IArgumentParser

---
+++ src/Remus/Parsing/Arguments/IArgumentParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Remus.Parsing.Arguments {
+    /// <summary>
+    /// Defines a contract for an argument parser.
+    /// </summary>
+    [PublicAPI]
+    public interface IArgumentParser {
+        /// <summary>
+        /// Gets the separator.
+        /// </summary>
+        string Separator { get; }
+        
+        /// <summary>
+        /// Gets the command name.
+        /// </summary>
+        string CommandName { get; }
+        
+        /// <summary>
+        /// Gets a read-only list of flags.
+        /// </summary>
+        IReadOnlyList<char> Flags { get; }
+
+        /// <summary>
+        /// Gets a read-only dictionary of options.
+        /// </summary>
+        IReadOnlyDictionary<string, object?> Options { get; }
+
+        /// <summary>
+        /// Parses a given input string.
+        /// </summary>
+        /// <param name="input">The string, which must not be <see langword="null"/>.</param>
+        void Parse([NotNull] string input);
+    }
+}
