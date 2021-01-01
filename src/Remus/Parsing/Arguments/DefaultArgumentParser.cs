579ba8f8f137f123fea2f6f4603d5dc55ec196bf
ivanbiljan <ssbiljan@gmail.com>
2021-01-02 00:06:45 +0100

    Add DefaultArgumentParser. TODO

---
+++ src/Remus/Parsing/Arguments/DefaultArgumentParser.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remus.Parsing.Arguments {
+    internal sealed class DefaultArgumentParser : IArgumentParser
+    {
+        /// <inheritdoc />
+        public string Separator { get; } = " ";
+
+        /// <inheritdoc />
+        public string? CommandName { get; private set; }
+
+        /// <inheritdoc />
+        public IReadOnlyList<string> Arguments { get; private set; } = new List<string>();
+
+        /// <inheritdoc />
+        public IReadOnlyList<char> Flags { get; private set; } = new List<char>();
+
+        /// <inheritdoc />
+        public IReadOnlyDictionary<string, string?> Options { get; private set; } = new Dictionary<string, string?>();
+
+        /// <inheritdoc />
+        public void Parse(string input, IReadOnlyCollection<string> availableCommandNames) {
+            if (string.IsNullOrWhiteSpace(input)) {
+                throw new ArgumentException("Input must not be null or empty.", nameof(input));
+            }
+
+            Debug.Assert(input.Length <= 1024);
+
+            var index = 0;
+            var tokens = TokenizeInput(input);
+            CommandName = ParseCommandName(availableCommandNames, tokens, ref index);
+            Arguments = ParseArguments(tokens, ref index);
+            Options = ParseOptionals(tokens, ref index);
+        }
+
+        private static string? ParseCommandName(
+            IReadOnlyCollection<string> availableCommandNames,
+            IReadOnlyList<string> tokens,
+            ref int index) {
+            // This performs a greedy match
+            Debug.Assert(tokens.Count > 0);
+
+            var commandName = default(string);
+            var i = 1;
+            var builder = new StringBuilder(tokens[0]);
+            do {
+                var tempCommand = builder.ToString();
+                if (!availableCommandNames.Contains(tempCommand)) {
+                    break;
+                }
+
+                commandName = tempCommand;
+                index = i;
+                builder.Append(" " + tokens.ElementAtOrDefault(i));
+            } while (i < tokens.Count);
+
+            return commandName;
+        }
+
+        private static Dictionary<string, string?> ParseOptionals(
+            IReadOnlyList<string> tokens,
+            ref int index) {
+            var currentOption = default(string);
+            var options = new Dictionary<string, string?>();
+            for (var i = index; i < tokens.Count; ++i) {
+                var token = tokens[index];
+                if (!token.StartsWith("-")) {
+                    // No options left to consume
+                    if (currentOption == default) {
+                        break;
+                    }
+
+                    options[currentOption] = token;
+                    currentOption = default;
+                    ++index;
+                    continue;
+                }
+
+                if (currentOption != default) {
+                    options[currentOption] = default;
+                }
+
+                if (!token.StartsWith("--")) {
+                    currentOption = token[1].ToString();
+                }
+                else {
+                    currentOption = token[2..];
+                    var indexOfEquals = currentOption.IndexOf('=');
+                    if (indexOfEquals > 0) {
+                        options[currentOption[..indexOfEquals]] = currentOption[(indexOfEquals + 1)..];
+                        currentOption = default;
+                    }
+                }
+
+                ++index;
+            }
+
+            return options;
+        }
+
+        private static List<string> ParseArguments(IReadOnlyList<string> tokens, ref int index) {
+            var arguments = new List<string>();
+            for (; index < tokens.Count; ++index) {
+                if (tokens[index][0] == '-') {
+                    break;
+                }
+
+                arguments.Add(tokens[index]);
+            }
+
+            return arguments;
+        }
+
+        /// <summary>
+        ///     Parses arguments from the specified input string. Supports quotation marks and escape characters.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns>A read-only list of tokens (arguments) extracted from the input string.</returns>
+        private static IReadOnlyList<string> TokenizeInput(string input) {
+            var parameters = new List<string>();
+            var stringBuilder = new StringBuilder(input.Length);
+            var inQuotes = false;
+            var isEscaped = false;
+            foreach (var currentCharacter in input) {
+                switch (currentCharacter) {
+                    case '\\':
+                        if (isEscaped) {
+                            stringBuilder.Append(currentCharacter);
+                            isEscaped = false;
+                            continue;
+                        }
+
+                        isEscaped = true;
+                        break;
+                    case ' ':
+                    case '\t':
+                    case '\n': {
+                            if (inQuotes || isEscaped) {
+                                stringBuilder.Append(currentCharacter);
+                                isEscaped = false;
+                            }
+                            else {
+                                if (stringBuilder.Length == 0) {
+                                    continue;
+                                }
+
+                                CommitPendingArgument();
+                            }
+
+                            break;
+                        }
+
+                    case '"': {
+                            if (isEscaped) {
+                                stringBuilder.Append(currentCharacter);
+                                isEscaped = false;
+                                continue;
+                            }
+
+                            inQuotes = !inQuotes;
+                            if (inQuotes) {
+                                continue;
+                            }
+
+                            CommitPendingArgument();
+                            break;
+                        }
+
+                    default:
+                        stringBuilder.Append(currentCharacter);
+                        break;
+                }
+            }
+
+            CommitPendingArgument();
+            return parameters;
+
+            void CommitPendingArgument() {
+                if (stringBuilder.Length == 0) {
+                    return;
+                }
+
+                parameters.Add(stringBuilder.ToString());
+                stringBuilder.Clear();
+            }
+        }
+    }
+}
--- src/Remus/Parsing/Arguments/IArgumentParser.cs
+++ src/Remus/Parsing/Arguments/IArgumentParser.cs
@@ -22,6 +22,11 @@ namespace Remus.Parsing.Arguments {
         string CommandName { get; }
         
         /// <summary>
+        /// Gets a read-only list of arguments.
+        /// </summary>
+        IReadOnlyList<string> Arguments { get; }
+
+        /// <summary>
         /// Gets a read-only list of flags.
         /// </summary>
         IReadOnlyList<char> Flags { get; }
@@ -29,12 +34,13 @@ namespace Remus.Parsing.Arguments {
         /// <summary>
         /// Gets a read-only dictionary of options.
         /// </summary>
-        IReadOnlyDictionary<string, object?> Options { get; }
+        IReadOnlyDictionary<string, string?> Options { get; }
 
         /// <summary>
-        /// Parses a given input string.
+        /// Parses a given input string against a given list of commands.
         /// </summary>
         /// <param name="input">The string, which must not be <see langword="null"/>.</param>
-        void Parse([NotNull] string input);
+        /// <param name="availableCommands">The list of available commands.</param>
+        void Parse([NotNull] string input, [NotNull] [ItemNotNull] IReadOnlyCollection<string> availableCommands);
     }
 }
