917437e63c9caad01e41f9d0e37e1c196673e71c
ivanbiljan <ssbiljan@gmail.com>
2021-01-22 01:27:47 +0100

    Remove ILGenerator.Extensions, add TrieNode test

---
+++ tests/Remus.Tests/TrieNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Remus.Tests {
+    public sealed class TrieNodeTests {
+        [Theory]
+        [InlineData('a')]
+        [InlineData('\0')]
+        [InlineData(' ')]
+        public void GetLetter_IsCorrect(char letter)
+        {
+            var node = new TrieNode(letter);
+
+            Assert.Equal(letter, node.Letter);
+        }
+    }
+}
