c8fe3f0c2a528cbfe659bad24ee17302fd56be5d
ivanbiljan <ssbiljan@gmail.com>
2021-01-22 16:30:46 +0100

    Add ConsoleSender tests

---
+++ tests/Remus.Tests/ConsoleSenderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Remus.Tests 
+{
+    public sealed class ConsoleSenderTests
+    {
+        [Fact]
+        public void SendMessage_NullMessage_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ConsoleSender().SendMessage(null!));
+        }
+        
+        [Fact]
+        public void SendMessage_IsCorrect()
+        {
+            var consoleSender = new ConsoleSender();
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            consoleSender.SendMessage("Hello, World");
+
+            Assert.Equal("Hello, World\r\n", output.ToString());
+        }
+    }
+}
