c4905318cdaea7fed25f1bd7dcbc827d5e50d263
ivanbiljan <ssbiljan@gmail.com>
2021-03-05 19:30:32 +0100

    Define a contract for results

---
+++ src/Remus/Results/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remus.Results {
+    /// <summary>
+    /// Defines a contract that describes the result of an action.
+    /// </summary>
+    public interface IResult {
+    }
+}
